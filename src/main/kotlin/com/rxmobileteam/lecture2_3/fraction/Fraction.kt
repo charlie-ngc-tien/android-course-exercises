f6eee08cc517606654764552de114c8f3c0f34f2
TienDucky <chitien173@gmail.com>
2024-03-28 20:58:00 +0700

    Implement fraction and product

--- src/main/kotlin/com/rxmobileteam/lecture2_3/fraction/Fraction.kt
+++ src/main/kotlin/com/rxmobileteam/lecture2_3/fraction/Fraction.kt
@@ -1,65 +1,93 @@
 package com.rxmobileteam.lecture2_3.fraction
 
+import java.lang.IllegalArgumentException
+import kotlin.math.roundToInt
+import kotlin.math.roundToLong
+
+fun calculateGCD(a: Int, b: Int): Int {
+  var num1 = a
+  var num2 = b
+  while (num2 != 0) {
+    val temp = num2
+    num2 = num1 % num2
+    num1 = temp
+  }
+  return num1
+}
+
 class Fraction private constructor(
   val numerator: Int,
   val denominator: Int,
 ) : Comparable<Fraction> {
   // TODO: Implement the decimal value of the fraction
-  val decimal: Double = TODO()
+  val decimal: Double = numerator.toDouble() / denominator
 
   init {
     // TODO: Check validity of numerator and denominator (throw an exception if invalid)
+    if (denominator == 0) {
+      throw IllegalArgumentException("denominator must be non zero.")
+    }
   }
 
   //region unary operators
   // TODO: "+fraction" operator
-  operator fun unaryPlus(): Fraction = TODO()
+  operator fun unaryPlus(): Fraction = Fraction(numerator, denominator)
 
   // TODO: "-fraction" operator
-  operator fun unaryMinus(): Fraction = TODO()
+  operator fun unaryMinus(): Fraction = Fraction(-numerator, -denominator)
   //endregion
 
   //region plus operators
   // TODO: "fraction+fraction" operator
-  operator fun plus(other: Fraction): Fraction = TODO()
+  operator fun plus(other: Fraction): Fraction = of(numerator * other.denominator + denominator * other.numerator, denominator * other.denominator)
 
   // TODO: "fraction+number" operator
-  operator fun plus(other: Int): Fraction = TODO()
+  operator fun plus(other: Int): Fraction = of(numerator + denominator * other, denominator)
   //endregion
 
   //region times operators
   // TODO: "fraction*fraction" operator
-  operator fun times(other: Fraction): Fraction = TODO()
+  operator fun times(other: Fraction): Fraction = of(numerator * other.numerator, denominator * other.denominator)
 
   // TODO: "fraction*number" operator
-  operator fun times(number: Int): Fraction = TODO()
+  operator fun times(number: Int): Fraction = of(numerator * number, denominator)
   //endregion
 
   // TODO: Compare two fractions
-  override fun compareTo(other: Fraction): Int = TODO()
+  override fun compareTo(other: Fraction): Int {
+    return when {
+      this == other -> 0
+      this.numerator * other.denominator > this.denominator * other.numerator -> 1
+      else -> -1
+    }
+  }
 
   //region toString, hashCode, equals, copy
   // TODO: Format the fraction as a string (e.g. "1/2")
-  override fun toString(): String = TODO()
+  override fun toString(): String = if (denominator == 1) "$numerator" else "$numerator/$denominator"
 
   // TODO: Implement hashCode
-  override fun hashCode(): Int = TODO()
+  override fun hashCode(): Int = decimal.hashCode()
 
   // TODO: Implement equals
-  override fun equals(other: Any?): Boolean = TODO()
+  override fun equals(other: Any?): Boolean{
+    if (other == null || other !is Fraction) {
+      return false
+    }
+    return other.decimal == decimal
+  }
 
   // TODO: Implement copy
   fun copy(
     numerator: Int = this.numerator,
     denominator: Int = this.denominator
-  ): Fraction = TODO()
+  ): Fraction = Fraction(numerator, denominator)
   //endregion
 
   companion object {
     @JvmStatic
     fun ofInt(number: Int): Fraction {
-      // TODO: Returns a fraction from an integer number
-      return Fraction(0, 0) // Change this
+      return Fraction(number, 1)
     }
 
     @JvmStatic
@@ -67,7 +95,11 @@ class Fraction private constructor(
       // TODO: Check validity of numerator and denominator
       // TODO: Simplify fraction using the greatest common divisor
       // TODO: Finally, return the fraction with the correct values
-      return Fraction(0, 0) // Change this
+      if (denominator == 0) {
+        throw IllegalArgumentException("denominator must be non zero.")
+      }
+      val gcd = calculateGCD(numerator, denominator)
+      return Fraction(numerator / gcd, denominator / gcd)
     }
   }
 }
@@ -77,30 +109,34 @@ infix fun Int.over(denominator: Int): Fraction = Fraction.of(this, denominator)
 
 //region get extensions
 // TODO: get the numerator, eg. "val (numerator) = Fraction.of(1, 2)"
-operator fun Fraction.component1(): Int = TODO()
+operator fun Fraction.component1(): Int = numerator
 
 // TODO: get the denominator, eg. "val (_, denominator) = Fraction.of(1, 2)"
-operator fun Fraction.component2(): Int = TODO()
+operator fun Fraction.component2(): Int = denominator
 
 // TODO: get the decimal, index must be 0 or 1
 // TODO: eg. "val numerator = Fraction.of(1, 2)[0]"
 // TODO: eg. "val denominator = Fraction.of(1, 2)[1]"
 // TODO: eg. "val denominator = Fraction.of(1, 2)[2]" should throw an exception
-operator fun Fraction.get(index: Int): Int = TODO()
+operator fun Fraction.get(index: Int): Int = when(index) {
+  0 -> numerator
+  1 -> denominator
+  else -> throw IllegalAccessException("Invalid index")
+}
 //endregion
 
 //region to number extensions
 // TODO: round to the nearest integer
-fun Fraction.roundToInt(): Int = TODO()
+fun Fraction.roundToInt(): Int = decimal.roundToInt()
 
 // TODO: round to the nearest long
-fun Fraction.roundToLong(): Long = TODO()
+fun Fraction.roundToLong(): Long = decimal.roundToLong()
 
 // TODO: return the decimal value as a float
-fun Fraction.toFloat(): Float = TODO()
+fun Fraction.toFloat(): Float = decimal.toFloat()
 
 // TODO: return the decimal value as a double
-fun Fraction.toDouble(): Double = TODO()
+fun Fraction.toDouble(): Double = decimal.toDouble()
 //endregion
 
 fun main() {
--- src/main/kotlin/com/rxmobileteam/lecture2_3/products/product.kt
+++ src/main/kotlin/com/rxmobileteam/lecture2_3/products/product.kt
@@ -25,35 +25,43 @@ data class Order(
 )
 
 // TODO: Return a list of Product, sorted in the ascending by price. if prices are equal, sorted by favoriteCount descending
-fun List<Product>.sortedByPriceAscendingThenByFavoriteCountDescending(): List<Product> = TODO()
+fun List<Product>.sortedByPriceAscendingThenByFavoriteCountDescending(): List<Product> = sortedByDescending { it.favoriteCount }
 
 // TODO: Return a set of Products in the orders (The order doesn't matter).
-fun List<Order>.getProductsSet(): Set<Product> = TODO()
+fun List<Order>.getProductsSet(): Set<Product> = flatMap { it.products }.toSet()
 
 // TODO: Return a list of Products in the orders, duplicates are allowed.
-fun List<Order>.getProductsList(): List<Product> = TODO()
+fun List<Order>.getProductsList(): List<Product> = flatMap { it.products }
 
 // TODO: Return a list of delivered orders
-fun List<Order>.getDeliveredOrders(): List<Order> = TODO()
+fun List<Order>.getDeliveredOrders(): List<Order> = filter { it.isDelivered }
 
 // TODO: Return a list of products in the delivered orders
-fun List<Order>.getDeliveredProductsList(): List<Product> = TODO()
+fun List<Order>.getDeliveredProductsList(): List<Product> = getDeliveredOrders().getProductsList()
 
 // TODO: Partition the orders into two lists: "delivered" and "not delivered"
-fun List<Order>.partitionDeliveredAndNotDelivered(): Pair<List<Order>, List<Order>> = TODO()
+fun List<Order>.partitionDeliveredAndNotDelivered(): Pair<List<Order>, List<Order>> = Pair(getDeliveredOrders(), filter { !it.isDelivered })
 
 // TODO: Return a map of product to count of this product in the orders
 // eg. [Product1 -> 2, Product2 -> 1, Product3 -> 3]
-fun List<Order>.countOfEachProduct(): Map<Product, Int> = TODO()
+fun List<Order>.countOfEachProduct(): Map<Product, Int> {
+  val setProduct = getProductsSet()
+  val productList = getProductsList()
+  return buildMap {
+    setProduct.forEach {
+      put(it, productList.count { product -> it == product })
+    }
+  }
+}
 
 // TODO: Return the sum of product prices in the order
-fun Order.sumProductPrice(): Double = TODO()
+fun Order.sumProductPrice(): Double = let { it.products.sumOf { product -> product.price }}
 
 // TODO: Return the product with the maximum price in the order
-fun Order.getMaxPriceProduct(): Product = TODO()
+fun Order.getMaxPriceProduct(): Product = let { it.products.maxBy { product->product.price } }
 
 // TODO: Return the product with the min price in the order
-fun Order.getMinPriceProduct(): Product = TODO()
+fun Order.getMinPriceProduct(): Product = let { it.products.minBy { product->product.price } }
 
 val product = Product(
   id = UUID.randomUUID().toString(),
